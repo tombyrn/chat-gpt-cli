65f02ac5a4d524913eeb1142c7df31ba9cf6faf3
tombyrn <thomasbyrnes02@gmail.com>
2023-03-16 18:38:28 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# chat-gpt-shell-
+# chat-gpt-cli
 Access ChatGPT through the Command Line
+
+## Setup
+Obtain a OpenAI API KEY. Create a ```env.js``` file in the root directory and put the following line in it ```module.exports = 'YOUR_OPEN_AI_KEY'```
+
+Use ``` npm link ``` to symlink package folder.
+```chat-gpt``` command should be available in your shell.
+
+## Example
+```
+$ chat-gpt how many inches in a mile
+===================== Chat-GPT =====================
+
+
+There are 63,360 inches in a mile.
+```
---
+++ chat-gpt.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+let prompt = "";
+for(let i = 2; i < process.argv.length; i++){
+    prompt += process.argv[i] + ' '
+}
+if(process.argv.length <= 2 || prompt.trim().length == 0){
+    console.error('No prompt provided to ChatGPT')
+    process.exit(1)
+}
+
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+    apiKey: require('./env'),
+});
+const openai = new OpenAIApi(configuration);
+
+async function generateText() {
+    const response = await openai.createCompletion({
+        model: 'text-davinci-002',
+        prompt: prompt,
+        temperature: 0,
+        max_tokens: 1000,
+    });
+
+    if(response.data.choices[0].text){
+        console.log('===================== Chat-GPT =====================')
+        console.log(response.data.choices[0].text)
+        console.log('\n')
+    }
+    else{
+        console.error('No response generated')
+    }
+
+}
+
+generateText();
